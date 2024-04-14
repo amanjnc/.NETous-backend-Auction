fe919717ce17ef11cdf7eeacce5cb34c78c56f3a
amanuel <amanuelbeyne662@gmail.com>
2024-04-14 14:26:13 +0300

    init

---
+++ Auctions/Models/Bid.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Auctions.Models
+{
+    public class Bid
+    {
+
+        public int id { get; set; }
+        public double price { get; set; }
+
+        [Required]
+        [ForeignKey("identityUserId")]
+        public string identityUserId { get; set; }
+        public IdentityUser user { get; set; }
+        
+        [ForeignKey("listingId")]
+        public int? listingId { get; set; }
+        public  Listing? listing { get; set; }
+    }
+}
